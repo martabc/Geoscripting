34078bcc5acd94fe45d3bcff92bc34f6ec51ccc5
martabc <marta.blancocastano@yahoo.com>
2017-05-10 17:06:47 -0600

    updates

--- Lesson 5/main.R
+++ Lesson 5/main.R
@@ -13,3 +13,12 @@
 # Find the place (i.e. a city) that intersects with this buffer.
 # Create a plot that shows the buffer, the points, and the name of the city
 # write down the name of the city and the population of that city as one comment at the end of the script.
+
+library(rgdal)
+library(sp)
+
+dsn = file.path("data","railways")
+rail <- readOGR(dsn, layer = ogrListLayers(dsn))
+
+plot(rail, col = "blue")
+summary(rail) ## gives a snapshot of the fields and first rows of data
