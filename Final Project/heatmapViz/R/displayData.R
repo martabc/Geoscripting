73280beb9eb68e33f74f37f23b4a41ddb2aa9058
martabc <marta.blancocastano@yahoo.com>
2017-06-05 18:50:13 -0600

    Final project

---
+++ Final Project/heatmapViz/R/displayData.R
@@ -0,0 +1,15 @@
+## First function -
+
+## Note: if an error message comes up saying there is no current rMaps version,
+## one may still install the library via the commands:
+## require(devtools)
+## install_github('ramnathv/rCharts@dev')
+## install_github('ramnathv/rMaps')
+# devtools::install_github('rstudio/leaflet')
+# devtools::install_github('bhaskarvk/leaflet.extras')
+
+library(rMaps)
+library(leaflet.extras)
+library(magrittr)
+
+
